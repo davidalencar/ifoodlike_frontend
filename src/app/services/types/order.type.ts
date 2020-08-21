e862e9c265422ff4dfe62b303420e04ebb7d47f6
davidalencar <davidalencarignacio@gmail.com>
2020-08-21 03:31:26 -0300

    merge bill ans customer

--- src/app/services/types/order.type.ts
+++ src/app/services/types/order.type.ts
@@ -3,6 +3,8 @@ import { AddressType } from './address.type';
 export class OrderType {
     paymMethod: string;
     instruction: string;
+    userName: string;
+    userPhone: string;
     address: AddressType;
 
     constructor() {
